d7d0c212e1f3d4f7541064993cba3abe832e9d96
butros01 <butrosmbuvi@gmail.com>
2023-09-29 22:35:53 +0300

    update home page

---
+++ lesson03/scripts/getdates.js
@@ -0,0 +1,14 @@
+/* VARIABLES */
+// Declare and instantiate a variable to hold the current year
+let currentYear = new Date().getFullYear()
+
+// Place the value of the current year variable into the HTML file
+
+document.querySelector('#year').textContent = currentYear
+
+// Declare and instantiate a variable to hold the last modified date
+
+let lastModified = new Date(document.lastModified)
+// Place the value of the last modified date variable into the HTML file
+
+document.querySelector('#lastModified').textContent = `Last Modification:  ${lastModified}`;
