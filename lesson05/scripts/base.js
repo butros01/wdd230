3eefaaf5047722d0cdafbc674887c184d7c04cb5
butros01 <butrosmbuvi@gmail.com>
2023-10-13 08:25:40 +0300

    complete BOM

---
+++ lesson05/scripts/base.js
@@ -0,0 +1,30 @@
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+
+
+button.addEventListener('click', () => { 
+    if (input.value !='') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener ('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.focus();
+        input.value = '';
+        }  
+    else {
+        alert("Name must be filled out")
+        input.focus();
+    } 
+    
+}) 
+
