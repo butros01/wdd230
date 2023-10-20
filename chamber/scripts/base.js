9f62543e4798904f5c62635062230158689135c4
butros01 <butrosmbuvi@gmail.com>
2023-10-21 01:42:54 +0300

    solve some bugs

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -10,21 +10,14 @@ hamButton.addEventListener('click', () => {
 var checkbox = document.getElementsByName("checkbox")[0];
 const main = document.querySelector("main");
 const body = document.querySelector('body')
-const h2 = document.querySelector('h2')
-const p = document.querySelector('section')
 
 checkbox.addEventListener("click", () => {
 	if(checkbox.checked) { 
 		body.style.backgroundColor = "#0A050F";
 		main.style.backgroundColor = "#0A050F";
-		main.style.Color = "#FFFFFF";
-		h2.style.color = "#FFFFFF";
-
 	}
 	else {
 		body.style.backgroundColor = "#FFFFFF";
 		main.style.backgroundColor = "#FFFFFF";
-		h2.style.color = "#0A050F";
-
 	}
 });
