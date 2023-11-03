a456d43ca4fe47d9d5f00d3a507dc2015c0f395b
butros01 <butrosmbuvi@gmail.com>
2023-11-03 10:56:07 +0300

    update forms

---
+++ scripts/form.js
@@ -0,0 +1,30 @@
+const pwd = document.querySelector("#pwd");
+const pwd2 = document.querySelector("#pwd2");
+const message = document.querySelector("#formmessage");
+
+pwd2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (pwd.value !== pwd2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		pwd2.style.backgroundColor = "#fff0f3";
+		pwd2.value = "";
+		pwd2.focus();
+	} else {
+		message.style.display = "none";
+		pwd2.style.backgroundColor = "#fff";
+		pwd2.style.color = "#000";
+	}
+}
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("rating");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
