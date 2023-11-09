18b6816c20e984ddc83fa6d96d290702de006796
butros01 <butrosmbuvi@gmail.com>
2023-11-09 21:57:29 +0300

    json

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,13 @@
 const baseURL = "https://butros01.github.io/wdd230/";
 const linksURL = "https://butros01.github.io/wdd230/data/links.json";
 
+const card = document.querySelector('.card');
+
 async function getLinks() {
     const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
+    const data = await response.json(); 
+    console.log(displayLinks(data.lessons));
+
   }
   
 getLinks();
@@ -12,13 +15,12 @@ getLinks();
 const displayLinks = (lessons) => {
     lessons.forEach((lesson) => {
         // Create elements to add to the div.cards element
-        let ul = document.createElement('ul');
         let week = document.createElement('li'); 
 
         // Build the li content 
-        week.textContent = `${lesson.lesson} ${lesson.links}`; 
+        week.textContent = `Week ${lesson.lesson} || ${lesson.links}`; 
 
         // Append the section(card) with the created elements
-        ul.appendChild(week); 
+        card.appendChild(week); 
     });
 }
