dcf07b1a7bc2909b3c8e736f635b48640cba65d5
butros01 <butrosmbuvi@gmail.com>
2023-11-15 12:27:51 +0300

    update form

--- scripts/form.js
+++ scripts/form.js
@@ -7,17 +7,19 @@ pwd2.addEventListener("focusout", checkSame);
 // This should be refactored.
 function checkSame() {
 	if (pwd.value !== pwd2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.textContent = "❗Password DO NOT MATCH!";
 		message.style.visibility = "show";
 		pwd2.style.backgroundColor = "#fff0f3";
 		pwd2.value = "";
 		pwd2.focus();
-	} else {
+	} 
+	else {
 		message.style.display = "none";
 		pwd2.style.backgroundColor = "#fff";
 		pwd2.style.color = "#000";
 	}
 }
+
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("rating");
 
