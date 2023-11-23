76d89108918ebb44e5dedd0ad1c881b1da170005
butros01 <butrosmbuvi@gmail.com>
2023-11-23 14:23:45 +0300

    update css

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,47 +1,39 @@
 const currentTemp = document.querySelector ('#current-temp'); 
 const weatherIcon = document.querySelector ('#weather-icon');
 const captionDesc= document.querySelector('figcaption');
-const currentTemp1 = document.querySelector ('#current-temp1'); 
-const weatherIcon1 = document.querySelector ('#weather-icon1');
-const captionDesc1= document.querySelector('figcaption1');
-const currentTemp2 = document.querySelector ('#current-temp2'); 
-const weatherIcon2 = document.querySelector ('#weather-icon2');
-const captionDesc2= document.querySelector('figcaption1');
-const currentTemp3 = document.querySelector ('#current-temp3'); 
-const weatherIcon3 = document.querySelector ('#weather-icon3');
-const captionDesc3= document.querySelector('figcaption3');
-// const card = document.querySelector('#weather') 
-// Declare a const variable named "url" and assign 
-// it a valid URL string as given in the openweathermap api documentation
+const forecastCont = document.querySelector(".forecast");
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=-1.29&lon=36.82&appid=2aed5d5a23adccd7c990dad86aeec570`
-const urlForcast = `https://api.openweathermap.org/data/2.5/forecast?lat=-1.29&lon=36.82&appid=2aed5d5a23adccd7c990dad86aeec570`
-async function apiFetch() {
+const url1 = `https://api.openweathermap.org/data/2.5/weather?lat=-1.29&lon=36.82&appid=2aed5d5a23adccd7c990dad86aeec570`
+const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=-1.29&lon=36.82&appid=2aed5d5a23adccd7c990dad86aeec570`
 
+
+async function apiFetch(url) {
     try {
-    
-        const response = await fetch (url);
-        const response2 = await fetch(urlForcast)
-        
-        if (response.ok) {
-            const data = await response.json(); 
-            displayResults (data); 
-        } 
-        else { throw Error (await response.text());
+        const response = await fetch (url1)
+        const response2 = await fetch(url2)
+        if(url= url1) {
+            
+            if (response.ok) {
+                const data = await response.json(); 
+                displayResults (data); 
+            } 
+            else { throw Error (await response.text());
+            }
         }
-        if (response2.ok) {
-            const data2 = await response2.json(); 
-            console.log(data2);
-            console.log(displayResults2(data2)); 
-        } 
-        else { throw Error (await response2.text());
+        if(url= url2) {
+            if (response2.ok) {
+                const data = await response2.json(); 
+                displayForecast(data);
+            } 
+            else { throw Error (await response2.text());
+            }
         }
     } 
     catch (error) {
         console.log(error);   
     }  
 }
-apiFetch();
+
 
 function displayResults (data) {
     let iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
@@ -51,20 +43,51 @@ function displayResults (data) {
     currentTemp.innerHTML = `${temp} - ${desc}`;   
 }
 
-function displayResults2 (data2) {
-    Array.from(data2.list, lists => {
-        let iconsrc1 = `https://openweathermap.org/img/w/${lists.weather[0].icon}.png`;
-        weatherIcon1.setAttribute('src',iconsrc1);
-        // weatherIcon2.setAttribute('src',iconsrc);
-        // weatherIcon3.setAttribute('src',iconsrc);
-        let date = lists.dt_txt
-        let desc = lists.weather[0].description;
-        let temp = `${lists.main.temp.toFixed(0) } &deg; F`;
-        forcast = `${date} ${temp} - ${desc}`
-        currentTemp1.innerHTML = `${date} ${temp} - ${desc}`; 
-        currentTemp2.innerHTML = `${date} ${temp} - ${desc}`; 
-        currentTemp3.innerHTML = `${date} ${temp} - ${desc}`; 
-        
+function displayForecast(data) {
+    const days = []
+    const dateNow = new Date();
+    const dayNow = dateNow.getDate();
+    let dayRange = 0;
+    let counter = 0;
+
+    //Source (Shecodes.com)
+    data.list.forEach((forecast) => {
+        const futureDate = new Date(forecast.dt * 1000);
+        const nextDay = futureDate.getDate();
+        if(dayNow !== nextDay && dayRange !== nextDay && counter <= 2) {
+            counter++;
+            dayRange = nextDay;
+            days.push(forecast);
+        }
     });
-    card.append(forcastlist);
+
+    days.forEach((item) => {
+        const iconsrc = `https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+        const container = document.createElement("div");
+        const date2 = document.createElement("p");
+        const temp2 = document.createElement("p");
+        const iconWeather = document.createElement("img");
+        const weatherDesc = document.createElement("p");
+        const newDate = new Date(item.dt * 1000);
+
+        date2.textContent = newDate.toLocaleString('default', {month: "short", day: "numeric"});
+        temp2.innerHTML = `${item.main.temp.toFixed(1)}&deg;F`;
+        iconWeather.setAttribute("src", iconsrc);
+        iconWeather.setAttribute("alt", item.weather[0].main);
+        container.append(date2, temp2, iconWeather, weatherDesc);
+        forecastCont.append(container);
+    });
+    
+    function calculateWindchill(num1, num2) {
+        if (num1 <= 50 && num2 > 3.0) {
+            let windChillFactor = 35.74 + (0.6215 * num1) - (35.75 * (num2 ** 0.16)) + ((0.4275 * num1) * (num2 ** 0.16));
+            windChill.textContent = windChillFactor.toFixed(2);
+        } else {
+            windChill.textContent = "N/A";
+        }
+    }
 }
+
+
+apiFetch(url1);
+
